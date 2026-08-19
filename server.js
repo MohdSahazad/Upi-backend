d9043bfe09083207ee950d1e10a5e09886ed31d7
MohdSahazad <159049044+MohdSahazad@users.noreply.github.com>
2026-08-19 17:24:02 +0530

    Remove body-parser import from server.js
    Removed the body-parser import statement.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const cors = require('cors');
 const QRCode = require('qrcode');
 const session = require('express-session');
